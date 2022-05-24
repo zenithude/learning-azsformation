80e02b7661f3309fefdb6818202a95f411842c14
zenithude <zenithude@protonmail.com>
2022-05-24 11:38:13 +0200

    add folder php/tp-boutique

---
+++ php/session_exemple/index.php
@@ -0,0 +1,33 @@
+<?php 
+// démarrer la session
+session_start();
+?>
+
+<!doctype html>
+<html lang="fr">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Style perso -->
+    <link rel="stylesheet" href="./css/style.css">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Tests de session</title>
+  </head>
+  <body>
+      <?php 
+      // Créeons 2 variables de sessions
+      $_SESSION["nom"] = "Doe";
+      $_SESSION["prenom"] = "John";
+      echo "Les variables de session ont été initialisée";
+      ?>
+    <h1>Accueil</h1>
+    <button><a href="./phpFiles/utiliserSession.php">Aller sur la page utiliserSession</a></button>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    
+  </body>
+  
+</html>
---
+++ php/tp-boutique/php/variables.php
@@ -0,0 +1,5 @@
+<?php 
+$title = "Cult'Arts";
+$author = "z3npr0g";
+$metaDesciption = "Une boutique qui regroupe des artistes de tout horizon";
+?>
